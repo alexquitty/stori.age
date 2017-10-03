5e138277bf2cb713029e1af3b817a9a230be7de7
AlexQ <alexquitty@gmail.com>
2017-10-03 16:34:01 +0300

    develop

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -23,11 +23,11 @@ return [
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-backend', /*'_identity-frontend',*/ 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],//['name' => '_identity-frontend', 'httpOnly' => true],
         ],
         'session' => [
             // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
+            'name' => 'advanced-backend',//'advanced-frontend',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
