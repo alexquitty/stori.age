6cdd2af6768db84d056221849e96bc874b84c170
AlexQ <alexquitty@gmail.com>
2017-10-04 17:31:28 +0300

    develop

--- backend/views/menu/index.php
+++ backend/views/menu/index.php
@@ -31,7 +31,7 @@ $this->params['breadcrumbs'][] = $this->title;
             'name',
             'icon',
             'ord',
-            //'content',
+            'content',
             //'access',
 
             ['class' => 'yii\grid\ActionColumn'],
