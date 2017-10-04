1ad8275841b2dc744efba6fd605bb49ab580bec1
AlexQ <alexquitty@gmail.com>
2017-10-04 18:16:15 +0300

    develop

---
+++ backend/models/MenuSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Menu;
+
+/**
+ * MenuSearch represents the model behind the search form of `common\models\Menu`.
+ */
+class MenuSearch extends Menu
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['code', 'parent_code', 'name', 'icon'], 'safe'],
+            [['ord', 'content', 'access'], 'integer'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Menu::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'ord' => $this->ord,
+            'content' => $this->content,
+            'access' => $this->access,
+        ]);
+
+        $query->andFilterWhere(['like', 'code', $this->code])
+            ->andFilterWhere(['like', 'parent_code', $this->parent_code])
+            ->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'icon', $this->icon]);
+
+        return $dataProvider;
+    }
+}
