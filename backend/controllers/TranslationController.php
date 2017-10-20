cde83abae622ccedc7db62cf6e9aca67ea068c44
AlexQ <alexquitty@gmail.com>
2017-10-20 13:49:29 +0300

    log

--- backend/controllers/TranslationController.php
+++ backend/controllers/TranslationController.php
@@ -4,23 +4,62 @@ namespace backend\controllers;
 
 use backend\traits\CRUDTrait;
 use Yii;
-use common\models\Message;
-use common\models\SourceMessage;
-use backend\models\TranslationSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
 
 /**
  * TranslationController implements the CRUD actions for SourceMessage model.
  */
 class TranslationController extends Controller
 {
-	use CRUDTrait;
+	use CRUDTrait {
+		CRUDTrait::init as public parentInit;
+	}
 
 
-	public $model = '';
+	public $model = 'SourceMessage';
+	public $model2 = 'Message';
 	public $searchModel = 'TranslationSearch';
+
+	/**
+	 * @param $model
+	 * @param $params
+	 *
+	 * @return bool
+	 */
+	protected function __afterModelSaved(&$model, &$params)
+	{
+		$result = false;
+
+		$model2 = $this->findModel2($model->id);
+		if(isset($model2))
+		{
+			$params[ucfirst($model2::tableName())]['id'] = $model->id;
+
+			if($model2->load($params) && $model2->save())
+			{
+				$this->viewParams = [
+					'model2' => $model2,
+				];
+				$result = true;
+			}
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @param $id
+	 *
+	 * @return mixed
+	 * @throws NotFoundHttpException
+	 */
+	protected function findModel2($id)
+	{
+		$modelName = $this->model2;
+
+		return $modelName::findOne($id);
+	}
 
     /**
      * Lists all SourceMessage models.
@@ -35,72 +74,14 @@ class TranslationController extends Controller
 	    ]);
     }
 
-    /**
-     * Creates a new SourceMessage model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
-    public function actionCreate()
-    {
-        $model = new SourceMessage();
-	    $model2 = new Message();
-
-        if($model->load($params = Yii::$app->request->post()) && $model->save())
-        {
-        	$params[ucfirst($model2::tableName())]['id'] = $model->id;
-	        if($model2->load($params)&& $model2->save())
-                return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('create', [
-            'model' => $model,
-	        'model2' => $model2,
-        ]);
-    }
-
-    /**
-     * Updates an existing SourceMessage model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
-    {
-        $model = $this->findModel($id);
-        $model2 = $this->findModel2($id);
-
-        if($model->load($params = Yii::$app->request->post()) && $model->save())
-        {
-	        $params[ucfirst(Message::tableName())]['id'] = $model->id;
-	        if($model2->load($params)&& $model2->save())
-                return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('update', [
-            'model' => $model,
-	        'model2' => $model2,
-        ]);
-    }
-
-    /**
-     * Deletes an existing SourceMessage model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param integer $id
-     * @return mixed
-     */
-    public function actionDelete($id)
-    {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['index']);
-    }
-
-	protected function findModel2($id)
+	/**
+	 *
+	 */
+	public function init()
 	{
-		if (($model = Message::findOne($id)) !== null) {
-			return $model;
-		}
+		if('\\' != $this->model2[0])
+			$this->model2 = $this->modelPrefix . $this->model2;
 
-		throw new NotFoundHttpException(Yii::t('cpanel', 'The requested page does not exist.'));
+		$this->parentInit();
 	}
 }
--- backend/traits/CRUDTrait.php
+++ backend/traits/CRUDTrait.php
@@ -22,6 +22,8 @@ trait CRUDTrait
 	protected $modelPrefix = '\common\models\\';
 	protected $searchModelPrefix = '\backend\models\\';
 
+	protected $viewParams = [];
+
 	/**
 	 * IMPORTANT! Must be defined in controller!
 	 */
@@ -41,7 +43,7 @@ trait CRUDTrait
 		$params = Yii::$app->request->post();
 		$this->__beforeActionChange($model, $params);
 
-		if($model->load($params) && $model->save())
+		if($model->load($params) && $model->save() && $this->__afterModelSaved($mode, $params))
 		{
 			$this->__logAction(); // save to log only info (content will in model itself)
 			$primaryKey = $model->primaryKey();
@@ -51,9 +53,12 @@ trait CRUDTrait
 		else
 			\func::d($model->errors);
 
-		return $this->render('create', [
-			'model' => $model,
-		]);
+		return $this->render('create', array_merge(
+			[
+				'model' => $model,
+			],
+			$this->viewParams
+		));
 	}
 
 	/**
@@ -74,34 +79,6 @@ trait CRUDTrait
 		$model->delete();
 
 		return $this->redirect(['index']);
-	}
-
-	/**
-	 * @param $id
-	 *
-	 * @return mixed
-	 */
-	protected function __actionUpdate($id)
-	{
-		/**
-		 * @var $model ActiveRecord
-		 */
-		$model = $this->findModel($id);
-		$params = Yii::$app->request->post();
-		$this->__beforeActionChange($model, $params);
-
-		if($model->load(\Yii::$app->request->post()))
-		{
-			$this->__logAction($model); // save to log & content
-			$primaryKey = $model->primaryKey()[0];
-
-			if($model->save())
-				return $this->redirect([ 'view', 'id' => $model->$primaryKey ]);
-		}
-
-		return $this->render('update', [
-			'model' => $model,
-		]);
 	}
 
 	/**
@@ -129,7 +106,54 @@ trait CRUDTrait
 	}
 
 	/**
-	 * @param $model \yii\
+	 * @param $id
+	 *
+	 * @return mixed
+	 */
+	protected function __actionUpdate($id)
+	{
+		/**
+		 * @var $model ActiveRecord
+		 */
+		$model = $this->findModel($id);
+		$params = Yii::$app->request->post();
+		$this->__beforeActionChange($model, $params);
+
+		if($model->load(\Yii::$app->request->post()))
+		{
+			$this->__logAction($model); // save to log & content
+			$primaryKey = $model->primaryKey()[0];
+
+			if($model->save() && $this->__afterModelSaved($model, $params))
+				return $this->redirect([ 'view', 'id' => $model->$primaryKey ]);
+		}
+
+		return $this->render('update', array_merge(
+			[
+				'model' => $model,
+			],
+			$this->viewParams
+		));
+	}
+
+	/**
+	 * Applies any operations that should be after model is saved (create or update).
+	 * Should return state of application. False will cause error and cease application run.
+	 *
+	 * @param $model
+	 * @param $params
+	 *
+	 * @return bool
+	 */
+	protected function __afterModelSaved(&$model, &$params)
+	{
+		return true;
+	}
+
+	/**
+	 * Changes $model and $params for any data pre-processing or anything like that.
+	 *
+	 * @param $model
 	 * @param $params
 	 */
 	protected function __beforeActionChange(&$model, &$params)
@@ -211,6 +235,22 @@ trait CRUDTrait
 	}
 
 	/**
+	 * Finds the Menu model based on its primary key value.
+	 * If the model is not found, a 404 HTTP exception will be thrown.
+	 * @param string $id
+	 * @return \yii\base\Model the loaded model
+	 * @throws \yii\web\NotFoundHttpException if the model cannot be found
+	 */
+	protected function findModel($id)
+	{
+		$modelName = $this->model;
+		if(($model = $modelName::findOne($id)) !== null)
+			return $model;
+
+		throw new NotFoundHttpException(Yii::t('cpanel', 'The requested page does not exist.'));
+	}
+
+	/**
 	 * @return mixed
 	 */
 	public function actionCreate()
@@ -272,22 +312,6 @@ trait CRUDTrait
 	public function behaviors()
 	{
 		return array_merge($this->__getBehaviorVerbs(), $this->__getBehaviorAccess());
-	}
-
-	/**
-	 * Finds the Menu model based on its primary key value.
-	 * If the model is not found, a 404 HTTP exception will be thrown.
-	 * @param string $id
-	 * @return \yii\base\Model the loaded model
-	 * @throws \yii\web\NotFoundHttpException if the model cannot be found
-	 */
-	protected function findModel($id)
-	{
-		$modelName = $this->model;
-		if(($model = $modelName::findOne($id)) !== null)
-			return $model;
-
-		throw new NotFoundHttpException(Yii::t('cpanel', 'The requested page does not exist.'));
 	}
 
 	/**
