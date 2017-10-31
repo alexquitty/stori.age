9d257351be57fc6abf3c12a35810244389ce16f2
AlexQ <alexquitty@gmail.com>
2017-10-31 14:10:57 +0300

    frontend

--- common/models/Entity.php
+++ common/models/Entity.php
@@ -6,11 +6,16 @@ namespace common\models;
  * This is the model class for table "entity".
  *
  * @property int $id
+ * @property string $cloned_id Ссылается на
+ * @property string $letter Буква
  * @property string $type_code Тип сущности
  * @property string $name Название
  * @property string $description Описание
  *
+ * @property Character[] $characters
  * @property EntityType $typeCode
+ * @property Entity $cloned
+ * @property Entity[] $entities
  */
 class Entity extends \yii\db\ActiveRecord
 {
@@ -28,13 +33,13 @@ class Entity extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-        	[['name'], 'required'],
-            [['letter', 'description'], 'string'],
-	        [['letter'], 'string', 'max' => 1],
+            [['cloned_id'], 'integer'],
+            [['description'], 'string'],
+            [['letter'], 'string', 'max' => 1],
             [['type_code'], 'string', 'max' => 150],
             [['name'], 'string', 'max' => 250],
-            [['type_code'], 'exist', 'skipOnError' => false, 'targetClass' => EntityType::className(), 'targetAttribute' => ['type_code' => 'code']],
-	        [['letter'], 'default'],
+            [['type_code'], 'exist', 'skipOnError' => true, 'targetClass' => EntityType::className(), 'targetAttribute' => ['type_code' => 'code']],
+            [['cloned_id'], 'exist', 'skipOnError' => true, 'targetClass' => Entity::className(), 'targetAttribute' => ['cloned_id' => 'id']],
         ];
     }
 
@@ -44,13 +49,38 @@ class Entity extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id' => 'ID',
-	        'letter' => 'Буква',
-            'type_code' => 'Тип сущности',
-            'name' => 'Название',
-            'description' => 'Описание',
+			'id' => 'ID',
+			'cloned_id' => 'Ссылается на',
+			'letter' => 'Буква',
+			'type_code' => 'Тип сущности',
+			'name' => 'Название',
+			'description' => 'Описание',
         ];
     }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCharacters()
+    {
+        return $this->hasMany(Character::className(), ['entity_id' => 'id']);
+    }
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getClones()
+	{
+		return $this->hasMany(Entity::className(), ['cloned_id' => 'id']);
+	}
+
+	/**
+	 * @return \yii\db\ActiveQuery
+	 */
+	public function getOriginal()
+	{
+		return $this->hasOne(Entity::className(), ['id' => 'cloned_id']);
+	}
 
     /**
      * @return \yii\db\ActiveQuery
@@ -58,5 +88,14 @@ class Entity extends \yii\db\ActiveRecord
     public function getTypeCode()
     {
         return $this->hasOne(EntityType::className(), ['code' => 'type_code']);
+    }
+
+    /**
+     * @inheritdoc
+     * @return EntityQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new EntityQuery(get_called_class());
     }
 }
---
+++ common/models/EntityQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace common\models;
+
+/**
+ * This is the ActiveQuery class for [[Entity]].
+ *
+ * @see Entity
+ */
+class EntityQuery extends \yii\db\ActiveQuery
+{
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * @inheritdoc
+     * @return Entity[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * @inheritdoc
+     * @return Entity|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+}
