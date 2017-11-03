11b2573d0f373789cd9664ce386aee0aa095d2ce
AlexQ <alexquitty@gmail.com>
2017-11-03 17:53:30 +0300

    frontend

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -13,7 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         // 'css/site.css',
-	    'css/custom.css',
+	    'css/custom.css?v=1',
     ];
     public $js = [
     	'js/script.js',
