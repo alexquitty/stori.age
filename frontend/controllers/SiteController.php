281da60fbacfc0a145528b1ee39bc1edec8123e5
AlexQ <alexquitty@gmail.com>
2019-02-27 09:45:17 +0300

    storiage

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -36,7 +36,7 @@ class SiteController extends Controller
                         'roles' => ['?'],
                     ],
                     [
-                        'actions' => ['logout'],
+                        'actions' => ['logout', 'index'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
